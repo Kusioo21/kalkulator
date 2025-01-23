1d425c486f64c76861d1dc19bc98c494fc1a45c0
Kusio21 <kusio360@gmail.com>
2025-01-23 02:48:29 +0100

    Kalkulator Poprawa

--- kalkulator/Form1.cs
+++ kalkulator/Form1.cs
@@ -5,7 +5,7 @@ namespace kalkulator
         private string firstCurrency;
         private string secondCurrency;
         private int amountValue;
-        private Dictionary<string, decimal> exchangeRates;
+        private Dictionary<string, double> exchangeRates;  
 
         public Form1()
         {
@@ -13,94 +13,29 @@ namespace kalkulator
             firstCurrency = "";
             secondCurrency = "";
             amountValue = 1;
-            exchangeRates = new Dictionary<string, decimal>
+            exchangeRates = new Dictionary<string, double>  
             {
-                { "PLNUSD", 0.26m },
-                { "PLNEUR", 0.23m },
-                { "PLNNOK", 2.74m },
-                { "PLNCHF", 0.22m },
-                { "USDPLN", 3.87m },
-                { "USDEUR", 0.90m },
-                { "USDNOK", 10.60m },
-                { "USDCHF", 0.85m },
-                { "EURPLN", 4.29m },
-                { "EURUSD", 1.11m },
-                { "EURNOK", 11.74m },
-                { "EURCHF", 0.94m },
-                { "NOKPLN", 0.37m },
-                { "NOKUSD", 0.094m },
-                { "NOKEUR", 0.085m },
-                { "NOKCHF", 0.080m },
-                { "CHFPLN", 4.58m },
-                { "CHFUSD", 1.18m },
-                { "CHFEUR", 1.07m },
-                { "CHFNOK", 12.54m },
+                { "PLNUSD", 0.26 },
+                { "PLNEUR", 0.23 },
+                { "PLNNOK", 2.74 },
+                { "PLNCHF", 0.22 },
+                { "USDPLN", 3.87 },
+                { "USDEUR", 0.90 },
+                { "USDNOK", 10.60 },
+                { "USDCHF", 0.85 },
+                { "EURPLN", 4.29 },
+                { "EURUSD", 1.11 },
+                { "EURNOK", 11.74 },
+                { "EURCHF", 0.94 },
+                { "NOKPLN", 0.37 },
+                { "NOKUSD", 0.094 },
+                { "NOKEUR", 0.085 },
+                { "NOKCHF", 0.080 },
+                { "CHFPLN", 4.58 },
+                { "CHFUSD", 1.18 },
+                { "CHFEUR", 1.07 },
+                { "CHFNOK", 12.54 },
             };
-        }
-
-        private void firstPLN_CheckedChanged(object sender, EventArgs e)
-        {
-            firstCurrency = "PLN";
-            wynikHeader.Text = firstCurrency + " - " + secondCurrency;
-        }
-
-        private void secondPLN_CheckedChanged(object sender, EventArgs e)
-        {
-            secondCurrency = "PLN";
-            wynikHeader.Text = firstCurrency + " - " + secondCurrency;
-        }
-
-        private void firstUSD_CheckedChanged(object sender, EventArgs e)
-        {
-            firstCurrency = "USD";
-            wynikHeader.Text = firstCurrency + " - " + secondCurrency;
-        }
-
-        private void secondUSD_CheckedChanged(object sender, EventArgs e)
-        {
-            secondCurrency = "USD";
-            wynikHeader.Text = firstCurrency + " - " + secondCurrency;
-        }
-
-        private void firstEUR_CheckedChanged(object sender, EventArgs e)
-        {
-            firstCurrency = "EUR";
-            wynikHeader.Text = firstCurrency + " - " + secondCurrency;
-        }
-
-        private void secondEUR_CheckedChanged(object sender, EventArgs e)
-        {
-            secondCurrency = "EUR";
-            wynikHeader.Text = firstCurrency + " - " + secondCurrency;
-        }
-
-        private void firstNOK_CheckedChanged(object sender, EventArgs e)
-        {
-            firstCurrency = "NOK";
-            wynikHeader.Text = firstCurrency + " - " + secondCurrency;
-        }
-
-        private void secondNOK_CheckedChanged(object sender, EventArgs e)
-        {
-            secondCurrency = "NOK";
-            wynikHeader.Text = firstCurrency + " - " + secondCurrency;
-        }
-
-        private void firstCHF_CheckedChanged(object sender, EventArgs e)
-        {
-            firstCurrency = "CHF";
-            wynikHeader.Text = firstCurrency + " - " + secondCurrency;
-        }
-
-        private void secondCHF_CheckedChanged(object sender, EventArgs e)
-        {
-            secondCurrency = "CHF";
-            wynikHeader.Text = firstCurrency + " - " + secondCurrency;
-        }
-
-        private void amount_ValueChanged(object sender, EventArgs e)
-        {
-            amountValue = (int)amount.Value;
         }
 
         private void calculateButton_Click(object sender, EventArgs e)
@@ -108,8 +43,8 @@ namespace kalkulator
             string key = firstCurrency + secondCurrency;
             if (exchangeRates.ContainsKey(key))
             {
-                decimal rate = exchangeRates[key];
-                decimal result = amountValue * rate;
+                double rate = exchangeRates[key];  
+                double result = amountValue * rate;  
                 wynikText.Text = $"{amountValue} {firstCurrency} = {result:F2} {secondCurrency}";
             }
             else
